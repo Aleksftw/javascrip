faca8b7a60d9429af04e168b2b6645fda9649a5a
Aleksandar Donev <donev92@abv.bg>
2017-07-13 14:58:36 +0100

    adding solution to the palindrome exercise

--- dist/basic.js
+++ dist/basic.js
@@ -28,13 +28,18 @@ factorialize(5); //returns 120
 
 
 function palindrome(str) {
-
+	var reversed = str.split('').reverse().join('');
+	if (reversed = str) {
+		return true;
+	} else {
+		return false;
+	}
 	return true;
 }
 
 
 
-palindrome("eye");
+palindrome("eye"); // returns true
 
 
 
